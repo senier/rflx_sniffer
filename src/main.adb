0497d38f42d8c1446f4a7c01b986e3b2eacf2541
Alexander Senier <senier@componolit.com>
2019-09-13 08:26:00 +0200

    Add specs and hexdump

--- src/main.adb
+++ src/main.adb
@@ -18,6 +18,31 @@ is
    Unused_Bit_Index : Types.Bit_Length_Type;
    use type Types.Bytes_Ptr;
    use type Types.Length_Type;
+
+   function Digit (D : Types.Byte) return Character
+   is
+      use type Types.Byte;
+   begin
+      if D < 10 then
+         return Character'Val (Types.Byte'Pos (D) + 48);
+      else
+         return Character'Val (Types.Byte'Pos (D) + 87);
+      end if;
+   end Digit;
+
+   procedure Dump (Buffer : Types.Bytes)
+   is
+      use type Types.Byte;
+   begin
+      for E in Buffer'Range
+      loop
+         if E /= Buffer'First then
+            Put (' ');
+         end if;
+         Put (Digit (Buffer (E) / 16) & Digit (Buffer (E) mod 16));
+      end loop;
+   end Dump;
+
 begin
    Network.Setup;
    if not Network.Valid
@@ -35,12 +60,20 @@ begin
          Put_Line ("Error reading packet");
       else
          Put_Line ("Got packet len:" & Last'Img);
+         Dump (Buffer (Buffer'First .. Last));
          IPv4.Packet.Initialize (Context, Buffer);
          IPv4.Packet.Verify_Message (Context);
+
+         for F in IPv4.Packet.F_Version .. IPv4.Packet.F_Payload loop
+            if not IPv4.Packet.Valid (Context, F) then
+               Put_Line ("Invalid " & F'Img);
+            end if;
+         end loop;
+
          if not IPv4.Packet.Valid_Message (Context) then
             Put_Line ("Message invalid");
          end if;
-         IPv4.Packet.Move_Buffer (Context, Buffer, Unused_Bit_Index);
+         IPv4.Packet.Take_Buffer (Context, Buffer, Unused_Bit_Index);
       end if;
    end loop;
 end Main;
