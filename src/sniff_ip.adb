abbccb056de8963ab63a640cd34e311eb546f378
Alexander Senier <senier@componolit.com>
2019-09-17 17:36:51 +0200

    Make code more slide friendly

--- src/sniff_ip.adb
+++ src/sniff_ip.adb
@@ -1,26 +1,26 @@
-with Sniffer.Types;
+with Sniffer.Types; use Sniffer.Types;
 with Sniffer.Raw;
 with Sniffer.Dump;
 with Sniffer.IPv4.Packet;
+use Sniffer.IPv4.Packet;
 with Sniffer.In_IPv4.Contains;
 with Sniffer.UDP.Datagram;
 
 procedure Sniff_IP
 is
-   use Sniffer;
-   package Network is new Raw (Element_Type => Types.Byte,
-                               Index_Type   => Types.Index_Type,
-                               Buffer_Type  => Types.Bytes);
-   Last    : Types.Index_Type;
+   package Network is new Sniffer.Raw
+      (Element_Type => Byte,
+       Index_Type   => Index_Type,
+       Buffer_Type  => Bytes);
+   Last    : Index_Type;
    Success : Boolean;
 
-   subtype Packet is Types.Bytes (1 .. 1500);
-   Buffer : Types.Bytes_Ptr := new Packet'(Packet'Range => 0);
+   subtype Packet is Bytes (1 .. 1500);
+   Buffer : Bytes_Ptr := new Packet'(Packet'Range => 0);
 
-   use type Types.Bytes_Ptr;
-   use type Types.Length_Type;
+   use type Bytes_Ptr; use type Length_Type;
 
-   Context : IPv4.Packet.Context_Type := IPv4.Packet.Create;
+   Context : Context_Type := Create;
 begin
    Network.Setup;
    if not Network.Valid
@@ -32,15 +32,15 @@ begin
       pragma Loop_Invariant (Buffer /= null and then Buffer'Last = Packet'Last);
       Network.Receive (Buffer.all, Last, Success);
       if Success then
-         IPv4.Packet.Initialize (Context, Buffer);
-         IPv4.Packet.Verify_Message (Context);
-         if IPv4.Packet.Structural_Valid_Message (Context) then
-            Dump.IP (Context);
-            if IPv4.Packet.Present (Context, IPv4.Packet.F_Payload) then
-               Dump.Payload (Context);
+         Initialize (Context, Buffer);
+         Verify_Message (Context);
+         if Structural_Valid_Message (Context) then
+            Sniffer.Dump.IP (Context);
+            if Present (Context, F_Payload) then
+               Sniffer.Dump.Payload (Context);
             end if;
          end if;
-         IPv4.Packet.Take_Buffer (Context, Buffer);
+         Take_Buffer (Context, Buffer);
       end if;
    end loop;
 end Sniff_IP;
